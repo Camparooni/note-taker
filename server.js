4ffa7eecbc4aa857c4b4ecdfe5b3d086772de898
Tay Campanelli <tayascamp@yahoo.com>
2023-11-06 21:28:03 -0800

    Added and updated additional files needed, server js and api routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fs = require("fs");
+const apiRoutes = require("./routes/apiroutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+    });
